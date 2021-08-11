d44a5e2220589ef3f57265fc34f35660ce3d553f
sunsheng.zhu <sunsheng.zhu@moco.com>
2021-08-11 13:42:58 +0800

    .

---
+++ src/com/common/GenericsUtils.java
@@ -0,0 +1,36 @@
+package com.common;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * 
+ * @Describe
+ * @author ZSS
+ * @date 2021年8月11日
+ * @time 下午1:35:17
+ */
+public class GenericsUtils {
+
+	@SuppressWarnings("rawtypes")
+	public static Class getSuperClassGenricType(Class clazz) {
+		return getSuperClassGenricType(clazz, 0);
+	}
+
+	@SuppressWarnings("rawtypes")
+	public static Class getSuperClassGenricType(Class clazz, int index) {
+		Type genType = clazz.getGenericSuperclass();
+		if (!(genType instanceof ParameterizedType)) {
+			return Object.class;
+		}
+		Type[] params = ((ParameterizedType) genType).getActualTypeArguments();
+		if (index >= params.length || index < 0) {
+			return Object.class;
+		}
+		if (!(params[index] instanceof Class)) {
+			return Object.class;
+		}
+		return (Class) params[index];
+	}
+	
+}
