598a7ae412566df994b9335ff6b92d6862c1711c
sunsheng.zhu <sunsheng.zhu@moco.com>
2022-01-11 15:45:28 +0800

    .

--- src/com/common/ClassLoaderUtil.java
+++ src/com/common/ClassLoaderUtil.java
@@ -1,6 +1,7 @@
 package com.common;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -28,4 +29,30 @@ public class ClassLoaderUtil {
 		}
 	}
 
+	@SuppressWarnings("unused")
+	private static void addDLLDir(String libraryPath) throws Exception {
+		try {
+			Field userPathsField = ClassLoader.class.getDeclaredField("usr_paths");
+			userPathsField.setAccessible(true);
+			String[] paths = (String[]) userPathsField.get(null);
+			StringBuilder sb = new StringBuilder();
+			String split = ("/".equals(File.separator) ? ":" : ";");
+			for (int i = 0; i < paths.length; i++) {
+				if (libraryPath.equals(paths[i])) {
+					continue;
+				}
+				sb.append(paths[i]).append(split);
+			}
+			sb.append(libraryPath);
+			// java.library.path, Like: -Djava.library.path={libraryPath}
+			System.setProperty("java.library.path", sb.toString());
+			final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+			sysPathsField.setAccessible(true);
+			// set sys_pathsnull
+			sysPathsField.set(null, null);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+	
 }
